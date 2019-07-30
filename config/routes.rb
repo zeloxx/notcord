176dfc1cda61cfb0dcc05e223cfe7dfc4a07fb21
Robert <robertyeakeltroidl@gmail.com>
2019-07-30 07:48:52 -0700

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: {format: :json} do
+    resource :session, only: [:create, :destroy]
+
+    resources :channels, only: [:create, :index, :destroy]
+    resources :direct_message_channel_users, only: [:create, :destroy]
+    resources :messages, only: [:create, :destroy, :update]
+    resources :servers, only: [:create, :index, :destroy]
+    resources :users, only: [:create]
+  end
+
+  root "static_pages#root"
 end
