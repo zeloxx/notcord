b5a745b5644d3cfa81d3b6284b1c22895a084a6b
Robert <robertyeakeltroidl@gmail.com>
2019-07-30 13:23:05 -0700

    added session form

---
+++ frontend/components/app/app_root.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AppRoot extends Component {
+    render() {
+        return (
+            <div>
+                rendering app root component
+            </div>
+        )
+    }
+}
--- frontend/notcord.jsx
+++ frontend/notcord.jsx
@@ -19,6 +19,10 @@ document.addEventListener('DOMContentLoaded', () => {
         store = configureStore();
     }
 
+    // FOR TESTING PURPOSES ONLY, DO NOT USE IN PRODUCTION
+    window.store = store
+    // 
+
     const root = document.getElementById('root');
     ReactDOM.render(<Root store={store} />, root);
 });
