68bbf9eb1c922384ceb3204aca8d797a323a72be
Robert <robertyeakeltroidl@gmail.com>
2019-07-30 13:02:21 -0700

    added entities, users, and root reducers

---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import entities from './entities/entities_reducer';
+import session from './session_reducer';
+
+const rootReducer = combineReducers({
+    entities,
+    session,
+});
+
+export default rootReducer;
