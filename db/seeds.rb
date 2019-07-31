a9a3143e0dd704ef3bf7749a8e286771824373d5
Robert <robertyeakeltroidl@gmail.com>
2019-07-31 13:35:13 -0700

    demo user seed fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(username: "jimothy", password: "password")
+User.create!(username: "jimothy", password: "password", email: "jimothy@jimothy.com")
